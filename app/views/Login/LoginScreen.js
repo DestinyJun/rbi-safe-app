90a732a5958f63e58eb835b80d1ba1dce609889c
DestinyJun <429771890@qq.com>
2020-07-09 23:24:06 +0800

    操作日志： （1）登录布局三 ------ 2020-07-09 23:23 ------

--- app/views/Login/LoginScreen.js
+++ app/views/Login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ImageBackground, Text, TouchableOpacity, View,} from 'react-native';
+import {ImageBackground, Text, View, Keyboard, KeyboardAvoidingView, Platform} from 'react-native';
 import {LoginStyles as styles} from './LoginStyles';
 // reducer
 import {Store} from "../../redux/store";
@@ -12,46 +12,97 @@ import {Button, Icon, Input} from "react-native-elements";
 export class LoginScreen extends Component {
   constructor(props) {
     super(props);
-    this.myRef = React.createRef();
+    this.myUsernameRef = React.createRef();
+    this.state = {
+      showPassword: false,
+      username: null,
+      password: null,
+    };
   }
+
   render() {
     return (
       <View style={[styles.login, c_styles.w_100, c_styles.dim_height]}>
-        <ImageBackground style={[styles.imageBgc]} source={IMAGE_FILE_LIST} resizeMode={'cover'}>
-          <View style={[styles.header]}>
-            <Text style={[c_styles.h2,c_styles.mb_3]}>欢迎登陆</Text>
-            <Text style={[c_styles.h4,]} allowFontScaling={true}>安全生产信息化平台</Text>
-          </View>
-          <View style={[styles.forms]}>
-            <Input
-              placeholder={'请输入账号'}
-              placeholderTextColor={'#BCBCBC'}
-              inputContainerStyle={{paddingBottom: 5,borderColor: '#DEDEDE'}}
-              leftIcon={<Icon type={'ionicon'} name={'ios-person'} color={'#BCBCBC'} size={20} />}
-              leftIconContainerStyle={{paddingRight: 10,paddingLeft: 4}}
-            />
-            <Input
-              placeholder={'请输入密码'}
-              secureTextEntry={true}
-              placeholderTextColor={'#BCBCBC'}
-              inputContainerStyle={{paddingBottom: 5,borderColor: '#DEDEDE'}}
-              leftIcon={<Icon type={'material'} name={'lock'} color={'#BCBCBC'} size={20} />}
-              leftIconContainerStyle={{paddingRight: 10}}
-              rightIcon={<Icon type={'ionicon'} name={'ios-eye-off'} color={'#BCBCBC'} size={20} />}
-            />
-          </View>
-          <View style={[styles.button]}>
-            <Button
-              onPress={this.login.bind(this)}
-              buttonStyle={{backgroundColor: '#337DF1',borderRadius: 30,paddingTop: 15,paddingBottom: 15}}
-              title="登录"/>
-          </View>
-        </ImageBackground>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{flex: 1}}>
+          <ImageBackground style={[styles.imageBgc]} source={IMAGE_FILE_LIST} resizeMode={'cover'}>
+            <View style={[styles.header]}>
+              <Text style={[c_styles.h2, c_styles.mb_3]}>欢迎登陆</Text>
+              <Text style={[c_styles.h4,]} allowFontScaling={true}>安全生产信息化平台</Text>
+            </View>
+            <View style={[styles.forms]}>
+              <Input
+                ref={this.myUsernameRef}
+                keyboardType={'email-address'}
+                placeholder={'请输入账号'}
+                placeholderTextColor={'#BCBCBC'}
+                onChangeText={(text) => {
+                  this.setState({username: text});
+                }}
+                inputContainerStyle={{paddingBottom: 5, borderColor: '#DEDEDE'}}
+                leftIcon={<Icon type={'ionicon'} name={'ios-person'} color={'#BCBCBC'} size={20}/>}
+                leftIconContainerStyle={{paddingRight: 10, paddingLeft: 4}}
+                rightIcon={this.state.username ?
+                  <Icon type={'ionicon'} name={'ios-close'} color={'#BCBCBC'} size={26} onPress={() => {
+                    this.myUsernameRef.current.clear();
+                    this.setState({username: null})
+                  }}/> : null}
+                rightIconContainerStyle={{paddingLeft: 16, paddingRight: 8}}
+              />
+              <Input
+                placeholder={'请输入密码'}
+                onChangeText={(text) => {
+                  this.setState({password: text})
+                }}
+                keyboardType={this.state.showPassword ? 'email-address' : 'default'}
+                secureTextEntry={!this.state.showPassword}
+                placeholderTextColor={'#BCBCBC'}
+                inputContainerStyle={{paddingBottom: 5, borderColor: '#DEDEDE'}}
+                leftIcon={<Icon type={'material'} name={'lock'} color={'#BCBCBC'} size={20}/>}
+                leftIconContainerStyle={{paddingRight: 10}}
+                rightIcon={<Icon type={'ionicon'} name={'ios-eye-off'} color={this.state.showPassword ? '#3782F8' : '#BCBCBC'} size={20} onPress={this.loginShowPasswordPress.bind(this)}/>}
+              />
+            </View>
+            <View style={[styles.button]}>
+              <Button
+                disabled={(!this.state.username || !this.state.password)}
+                disabledStyle={{backgroundColor: '#8EB9F5'}}
+                disabledTitleStyle={{color: '#ffffff'}}
+                onPress={this.login.bind(this)}
+                buttonStyle={{backgroundColor: '#337DF1', borderRadius: 30, paddingTop: 15, paddingBottom: 15}}
+                title="登录"
+                titleStyle={{letterSpacing: 5, fontSize: 18}}
+              />
+            </View>
+          </ImageBackground>
+        </KeyboardAvoidingView>
       </View>
     );
   }
+
+  // 组件挂载前执行
+  componentDidMount() {
+    // 监听键盘隐藏
+    Keyboard.addListener('keyboardDidHide', (event) => {
+      console.log(event);
+    });
+  }
+
+  // 登录操作
   login() {
     Store.dispatch(isLoading({type: ISLOADING, isLoading: true}))
+  }
+
+  // 密码显示隐藏切换事件
+  loginShowPasswordPress() {
+    this.setState({
+      showPassword: !this.state.showPassword
+    });
+  }
+
+  // 组件销毁时生命周期
+  componentWillUnmount(): void {
+    // 组件销毁时移除键盘监听事件
+    Keyboard.removeAllListeners('keyboardDidHide');
   }
 }
 
